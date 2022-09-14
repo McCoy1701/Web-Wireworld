18fc80c94bce5cd4109650a411a6a7e258a2bfbb
McCoy1701 <91435818+McCoy1701@users.noreply.github.com>
2022-09-13 20:47:18 -0400

    added gifs

--- makeGif.py
+++ makeGif.py
@@ -1,15 +1,16 @@
-import glob, os
+import glob, os, imageio
 from PIL import Image
 
 def makeGif(frameFolder, name):
-    frame = [Image.open(image) for image in glob.glob(f'{frameFolder}/*.PNG')]
-    frameOne = frame[0]
-    frameOne.save('gifs/' + name + '.gif', format='GIF', append_images=frame, save_all=True, duration=100, loop=0)
-
+    with imageio.get_writer('/path/to/movie.gif', mode='I') as writer:
+        for filename in filenames:
+            image = imageio.imread(filename)
+            writer.append_data(image)
+            
 def walker(folder):
     folders = os.listdir(folder)
     for dir in folders:
         makeGif(folder + dir + '/', dir)
 
 if __name__ == '__main__':
-    walker('animations/')
+    makeGif('animations/andGate/', 'andGate')
